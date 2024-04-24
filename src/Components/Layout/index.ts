2f40bf30029863b4e2dd5c849af5026037ccaa08
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-04-24 14:22:00 -0400

    8

--- src/Components/Layout/index.ts
+++
@@ -1,6 +0,0 @@
-import Nav from "./header/Nav";
-import Footer from "./footer/Footer";
-import Header from "./header/Header";
-import Banner from "./header/Banner";
-
-export{Nav, Footer, Header, Banner}
