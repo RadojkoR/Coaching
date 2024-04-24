1ca35f38a45e00c923e7bcb81e0953fc8639128c
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-04-24 13:12:17 -0400

    22222

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: 'https://radoyko.com/my_project/coaching',
   plugins: [react()],
 })
